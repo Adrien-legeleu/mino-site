f1039a13a839802c13319261964a460798db31f5
Test <adrienlegeleu@gmail.com>
2024-04-03 22:30:43 +0200

    mino

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -4,9 +4,6 @@ import Form from "./Form";
 const Contact = () => {
 
     const [isVisibleForm , setIsVisibleForm] =useState(false)
-    const openForm=()=>{
-        setIsVisibleForm(true)
-    }
 
   return (
     <div className="bg-grayLight shadow-contact_shadow pt-48 grid grid-cols-30/70 pl-10 pr-10 pb-32">
@@ -22,34 +19,35 @@ const Contact = () => {
         </h2>
         <p className="text-5xl mb-16 text-white leading-tight">Nous sommes toujours en quête de nouveautés et de nouvelles rencontres.</p>
         {contactText.map((desc, index) => (
-          <div
-            className="grid grid-cols-90/10 p-10 h-48 w-full bg-grayDark  justify-center rounded-3xl group relative mb-4"
-            key={index}
-          >
-            <div className="bg-[rgb(95,103,114)] opacity-0 rounded-3xl w-full h-full absolute top-0 left-0 group-hover:opacity-100 duration-300 ease-in-out "></div>
-            <div className="grid grid-cols-20/80 gap-8 group-hover:translate-x-5 duration-500 ease-in-out z-10">
-              <div className="bg-[rgba(30,30,30,0.4)] rounded-3xl h-full w-full flex items-center justify-center ">
-                <img className="w-2/3" src={desc.icon} alt={`icon : ${index}`} />
-              </div>
-              <div className="flex items-center ">
-                <p className="text-whiteGray text-3xl ">
-                  {desc.text}
-                </p>
-              </div>
-            </div>
-            <div className="flex items-center justify-end z-10">
-              <button className="bg-green  pt-3 pb-3 pr-5 pl-5 rounded-full opacity-0 translate-y-12 group-hover:opacity-100 group-hover:translate-y-0 duration-500 ease-in-out hover:brightness-90 hover:scale-110 " onClick={openForm}>
-                <img
-                  className="w-4"
-                  src="./icon/right-arrow.png"
-                  alt="right arrow"
-                />
-              </button>
-            </div>
-          </div>
-        ))}
+  <div
+    className="grid grid-cols-90/10 p-10 h-48 w-full bg-grayDark justify-center rounded-3xl group relative mb-4"
+    key={index}
+  >
+    <div className="bg-[rgb(95,103,114)] opacity-0 rounded-3xl w-full h-full absolute top-0 left-0 group-hover:opacity-100 duration-300 ease-in-out "></div>
+    <div className="grid grid-cols-20/80 gap-8 group-hover:translate-x-5 duration-500 ease-in-out z-10">
+      <div className="bg-[rgba(30,30,30,0.4)] rounded-3xl h-full w-full flex items-center justify-center ">
+        <img className="w-2/3" src={desc.icon} alt={`icon : ${index}`} />
       </div>
-      <Form setIsVisibleForm={setIsVisibleForm} isVisibleForm={isVisibleForm}/>
+      <div className="flex items-center ">
+        <p className="text-whiteGray text-3xl ">
+          {desc.text}
+        </p>
+      </div>
+    </div>
+    <div className="flex items-center justify-end z-10">
+      <button className="bg-green pt-3 pb-3 pr-5 pl-5 rounded-full opacity-0 translate-y-12 group-hover:opacity-100 group-hover:translate-y-0 duration-500 ease-in-out hover:brightness-90 hover:scale-110 " onClick={()=>setIsVisibleForm(true)}>
+        <img
+          className="w-4"
+          src="./icon/right-arrow.png"
+          alt="right arrow"
+        />
+      </button>
+    </div>
+    <Form setIsVisibleForm={setIsVisibleForm} isVisibleForm={isVisibleForm} desc={desc} />
+  </div>
+))}
+
+      </div>
     </div>
   );
 };
@@ -58,14 +56,20 @@ export default Contact;
 const contactText = [
   {
     text: "J'ai un projet précis, je souhaite obtenir un devis.",
-    icon:"./icon/file (1).png"
+    text2: "Vous avez déja une idéé et des informations ?",
+    under_text2:"Écrivez-nous pour prendre contact, nous allons échanger sur votre besoin et le chiffrer.",
+    icon:"./icon/file (1).png",
   },
   {
     text: "J'ai besoin de plus d'informations sur Mino.",
-    icon:"./icon/info.png"
+    text2: "Des questions à nous poser ?",
+    under_text2:"Posez-nous votre question , nous vous répondrons dans le plus de détails",
+    icon:"./icon/info.png",
   },
   {
     text: "Votre agence m'intéresse et je souhaite postuler.",
-    icon:"./icon/user.png"
+    text2: "Notre agence vous intéresse ?",
+    under_text2:"Très bon choix, vous pouvez nous contacter directement ci-dessous.",
+    icon:"./icon/user.png",
   },
 ];
--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,66 @@
-import { Transition } from "@tailwindui/react";
-
 interface FormProps {
-    isVisibleForm : boolean;
-    setIsVisibleForm:React.Dispatch<React.SetStateAction<boolean>>
+  isVisibleForm: boolean;
+  setIsVisibleForm: React.Dispatch<React.SetStateAction<boolean>>;
+  desc: {
+    text: string;
+    text2: string;
+    under_text2: string;
+    icon: string;
+  };
 }
 
-const Form : React.FC<FormProps> = ({ isVisibleForm, setIsVisibleForm }) => {
-    const closeForm = () => {
-        setIsVisibleForm(false);
-    };
+const Form: React.FC<FormProps> = ({
+  isVisibleForm,
+  setIsVisibleForm,
+  desc,
+}) => {
+  const closeForm = () => {
+    setIsVisibleForm(false);
+  };
 
-    return (
-        <Transition
-            show={isVisibleForm}
-            enter="transition-opacity duration-500"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition-opacity duration-500"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
+  return (
+    <div>
+      <div
+        className={`h-screen w-screen fixed top-0 left-0  z-[100] duration-500 ease-in-out ${
+          isVisibleForm ? "visible" : "invisible"
+        }`}
+      >
+        <div
+          className={`h-full w-full bg-[rgba(0,0,0,0.14)] backdrop-blur-md duration-300 ease-in-out ${
+            isVisibleForm ? " opacity-100 " : " opacity-0 "
+          }`}
+          onClick={closeForm}
+        ></div>
+        <div
+          className={`h-[95%] w-2/5 bg-grayDark absolute top-1/2 right-10 rounded-3xl -translate-y-1/2 flex flex-col pb-20 pt-24 pl-16 pr-16  shadow-containerService duration-500 ease-in-out overflow-y-auto ${
+            isVisibleForm ? "translate-x-0" : "translate-x-[150%] right-0"
+          }`}
         >
-            {(ref) => (
-                <div
-                    ref={ref}
-                    className={`h-screen w-screen fixed top-0 left-0 z-[100] duration-500 ease-in-out ${isVisibleForm ? "block" : "hidden"}`}
-                >
-                    <div className={`h-full w-full bg-blackTransparent backdrop-blur-md duration-500 ease-in-out ${isVisibleForm ? "block" : "hidden"}`} onClick={closeForm}></div>
-                    <div className={`h-[95%] w-2/5 bg-grayDark absolute top-1/2 right-10 rounded-3xl -translate-y-1/2  shadow-containerService duration-500 ease-in-out ${isVisibleForm ? "translate-x-0" : "translate-x-[150%] right-0"}`}></div>
-                </div>
-            )}
-        </Transition>
-    );
+          <div className="bg-[rgba(30,30,30,0.4)] rounded-3xl flex min-h-24 min-w-24 w-24 items-center justify-center mb-8">
+            <img className="w-14" src={desc.icon} alt="icon" />
+          </div>
+          <div className=" mb-10 flex flex-col gap-4">
+            <p className="text-2xl text-white">{desc.text2}</p>
+            <p className="text-whiteGray">{desc.under_text2}</p>
+          </div>
+          <div>
+            <p className="text-end text-white">* champs obligatoires</p>
+          </div>
+          <form className="mt-16 flex flex-col gap-8">
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                placeholder="Standard"
+                className="peer h-full w-full border-b border-whiteGray bg-transparent pt-4 pb-1.5 font-sans text-sm font-normal text-white outline outline-0 transition-all placeholder-shown:border-whiteGra focus:border-gray-500 focus:outline-0 disabled:border-0 disabled:bg-whiteGray placeholder:opacity-0 focus:placeholder:opacity-100"
+              />
+              <label className="after:content[''] pointer-events-none absolute left-0  -top-1.5 flex h-full w-full select-none !overflow-visible truncate text-[11px] font-normal leading-tight text-gray-500 transition-all after:absolute after:-bottom-1.5 after:block after:w-full after:scale-x-0 after:border-b-2 after:border-gray-500 after:transition-transform after:duration-300 peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.25] peer-placeholder-shown:text-whiteGray peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-green peer-focus:after:scale-x-100 peer-focus:after:border-green peer-focus:after:rounded-full peer-disabled:text-transparent peer-disabled:peer-placeholder-shown:text-white">
+                Standard
+              </label>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Form;
